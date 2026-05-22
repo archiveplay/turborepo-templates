a77a62aacbc62d256ad545b3f8d1b3893ee84813
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-22 14:40:59 +0300

    feat(apps/consumer): init bot service

--- apps/consumer/src/app.module.ts
+++ apps/consumer/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AllExceptionsFilter } from '@repo/nest-extensions/filters/all-exceptions';
 import { APP_FILTER } from '@nestjs/core';
+import { TgService } from './tg/tg.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
     AppService,
+    TgService,
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
