875e9a22646b7b35c2be1fb48aa01ffc9f0334a0
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-22 15:10:24 +0300

    fix(packages/config): update shape tg .env

--- packages/config/env/tg.ts
+++ packages/config/env/tg.ts
@@ -4,7 +4,8 @@ import { z } from "zod";
 dotenv.config();
 
 export const TgSchema = z.object({
-  TG_BOT_TOKEN: z.url(),
+  TG_BOT_TOKEN: z.string(),
+  TG_CHAT_ID: z.string(),
 });
 
 export const env = TgSchema.parse(process.env);
