e65c1350d22cf1a9d765597c1905c1d3ca4a586c
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-23 13:33:29 +0300

    refactor(apps/api): get user by id with exception filter

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,17 +1,31 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { PingReqSchema, PingResSchema } from '@repo/api/ping';
 import { createZodDto, ZodResponse } from 'nestjs-zod';
-import { UsersResSchema } from '@repo/api/user';
+import { UserResSchema, UsersResSchema } from '@repo/api/user';
 
 class PingReqDto extends createZodDto(PingReqSchema) {}
 class PingResDto extends createZodDto(PingResSchema) {}
 
 class UsersResDto extends createZodDto(UsersResSchema) {}
+class UserResDto extends createZodDto(UserResSchema) {}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('users/:id')
+  @ZodResponse({ type: UserResDto })
+  userById(@Param('id', ParseIntPipe) id: number) {
+    return this.appService.getUserById(id);
+  }
 
   @Get('users')
   @ZodResponse({ type: UsersResDto })
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { UsersResType } from '@repo/api/user';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UsersResType, UserResType } from '@repo/api/user';
 import { prisma } from '@repo/db';
 
 @Injectable()
 export class AppService {
+  async getUserById(id: number): Promise<UserResType> {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
   getUsers(): Promise<UsersResType> {
     return prisma.user.findMany();
   }
