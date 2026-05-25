41f3b0a7ab8f7c463c422012441f96de9df6f84c
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-25 12:16:44 +0300

    refactor(packages/db): using config package env

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient, Prisma } from "@prisma/client";
+import { env } from "@repo/config/env/server";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
