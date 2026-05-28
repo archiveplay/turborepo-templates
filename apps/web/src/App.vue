4bae5098324ed49dbf8b7229ae4fb2e75d1fa91d
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-28 21:36:37 +0300

    feat(apps/web): sdk integrated

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserControllerUsers } from "@repo/api/client"
+
+const { data, isLoading } = useUserControllerUsers()
+</script>
 
 <template>
   <h1>You did it!</h1>
+  <span v-if="isLoading">Loading...</span>
+  <pre v-else-if="data">{{ data }}</pre>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import { configureClient } from '@repo/api/config'
+import { env } from '@repo/config/env/vite'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +11,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
+
+configureClient({
+  baseUrl: env.VITE_API_URL,
+})
 
 app.mount('#app')
