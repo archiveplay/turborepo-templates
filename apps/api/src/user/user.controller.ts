4a24fe079e46c64aa3cacc53e491a316ae07c224
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-28 19:26:52 +0300

    feat(apps/api): user control api ok response shape

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ZodResponse } from 'nestjs-zod';
 import { UserResDto, UsersResDto } from 'src/common/dto';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller()
 export class UserController {
@@ -9,12 +10,14 @@ export class UserController {
 
   @Get('users/:id')
   @ZodResponse({ type: UserResDto })
+  @ApiOkResponse({ type: UserResDto })
   userById(@Param('id', ParseIntPipe) id: number) {
     return this.userService.getUserById(id);
   }
 
   @Get('users')
   @ZodResponse({ type: UsersResDto })
+  @ApiOkResponse({ type: UsersResDto })
   users() {
     return this.userService.getUsers();
   }
