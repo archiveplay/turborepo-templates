fbf4aa3d3eacb0dcac80fe6e2e3b89ed1f342f12
Vladislav Nefedov <clay.zenx@gmail.com>
2026-05-31 13:56:24 +0300

    feat(packages/config): tsconfig for packages/config

--- packages/config/env/server.ts
+++ packages/config/env/server.ts
@@ -1,12 +1,16 @@
-import dotenv from "dotenv";
+import { configDotenv } from "dotenv";
 import { z } from "zod";
 
-dotenv.config();
+configDotenv();
 
 export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).optional(),
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.url(),
+
+  REDIS_HOST: z.string().default("localhost"),
+  REDIS_PORT: z.coerce.number().default(6379),
+  CACHE_TTL: z.coerce.number().default(60),
 });
 
 export const env = serverSchema.parse(process.env);
