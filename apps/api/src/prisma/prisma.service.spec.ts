9d4e71ac5592874c15f797e573789515d7753aa8
Vladislav Nefedov <clay.zenx@gmail.com>
2026-06-01 17:22:42 +0300

    test(apps/api): test get back

---
+++ apps/api/src/prisma/prisma.service.spec.ts
@@ -0,0 +1,23 @@
+import { PrismaService } from './prisma.service';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(() => {
+    service = new PrismaService();
+  });
+
+  it('should exist', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should call $disconnect on destroy', async () => {
+    const disconnectSpy = jest
+      .spyOn(service, '$disconnect')
+      .mockResolvedValue(undefined);
+
+    await service.onModuleDestroy();
+
+    expect(disconnectSpy).toHaveBeenCalled();
+  });
+});
